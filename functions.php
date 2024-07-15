7adb37d685ca118e4d713fca7f51fffaf543926e
pr0pz <info@propz.de>
2024-07-15 10:54:18 +0200

    version 1.0.2 - Improved: renamed style.scss to style.css so directly usable with WP - Added: enqueued style.css on front and backend - Updated: gitignore

--- functions.php
+++ functions.php
@@ -6,3 +6,11 @@
 // https://developer.wordpress.org/block-editor/how-to-guides/themes/theme-support/
 add_theme_support( 'block-template-parts' );
 add_theme_support( 'wp-block-styles' );
+
+// Load style.css on front and backend
+function prpz_enqueue_stylesheet()
+{
+	wp_enqueue_style( 'style', get_stylesheet_uri() );
+}
+add_action( 'wp_enqueue_scripts', 'prpz_enqueue_stylesheet' );
+add_action( 'admin_enqueue_scripts', 'prpz_enqueue_stylesheet' );
